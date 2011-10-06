581a2f40d8766f45cb5d160cc534b5239676f0e1
sqiu <sqiu@arcsoft.com>
2011-10-07 03:17:58 +0800

    init Signed-off-by: sqiu <sqiu@arcsoft.com>

---
+++ MiniBlog/MiniBlog/stdafx.cpp
@@ -0,0 +1,9 @@
+
+// stdafx.cpp : source file that includes just the standard includes
+// MiniBlog.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
+#pragma comment(lib,"./sqlite/lib/sqlite3.lib")
